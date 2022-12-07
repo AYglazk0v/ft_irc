4e41d8e1b2410feed6116d2558c9b8be0bab8f2f
Goblin Tagganaro <AYGlazkov@stud.kpfu.ru>
2022-12-07 16:38:57 +0300

    7.12 continue write project

--- includes/User.hpp
+++ includes/User.hpp
@@ -9,10 +9,24 @@ class User {
 		int 				sock_fd_;
 		struct sockaddr_in	addres_;
 
+		bool				access_;
+		bool				autorization_;
+		bool				away_;
+		bool				oper_;
+		bool				invis_;
+		
+		std::string			nick_;
+		std::string			user_;
+		std::string			host_;
+		std::string			serverName_;
+		std::string			realName_;
+		std::string			buff_;
+		std::string			away_msg_;
+
 	public:
 		User(int id, int sock_fd, sockaddr_in addres);
 		int getSockFd() { return sock_fd_; }
-		
+
 };//User
 
 #endif
--- src/user.cc
+++ src/user.cc
@@ -1,5 +1,9 @@
 #include "../includes/User.hpp"
 
 User::User(int id, int sock_fd, sockaddr_in addres) : id_(id), sock_fd_(sock_fd), addres_(addres) {
-
+	access_ = 0;
+	autorization_ = 0;
+	away_ = 0;
+	oper_ = 0;
+	invis_ = 0;
 }
