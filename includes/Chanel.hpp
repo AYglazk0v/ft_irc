05ddad6c574565981e2ea7b224a9a91beebf60cb
ayglazkov <AYGlazkov@stud.kpfu.ru>
2022-12-10 13:41:39 +0300

    add functions is... in Chanel.hpp

--- includes/Chanel.hpp
+++ includes/Chanel.hpp
@@ -18,18 +18,22 @@ class Chanel {
 		bool				invOnly_;
 		std::vector<User>	members_;
 		std::vector<User>	operators_;
-		std::vector<User>	ivited_;
-		std::vector<User>	invisables_;
-		std::vector<User>	baned_;
+		std::vector<User>	invited_;
+		std::vector<User>	invisibles_;
+		std::vector<User>	banned_;
 		std::string			topic_;
 	public:
 		Chanel(User *user, std::string name, std::string password);
 		
-		std::string getName()		{ return name_; }
-		std::string getPassword()	{ return password_; }
-		std::string getTopic()		{ return topic_;}
+		std::string getName() const		{ return name_; }
+		std::string getPassword() const	{ return password_; }
+		std::string getTopic() const	{ return topic_;}
 
 		bool isMember(User *usr) const { return (members_.end() != std::find_if(members_.begin(), members_.end(), [&usr](const User& curr_cmp) { return usr->getId() == curr_cmp.getId(); })); }
+		bool isOperators(User *usr) const { return (operators_.end() != std::find_if(operators_.begin(), operators_.end(), [&usr](const User& curr_cmp) { return usr->getId() == curr_cmp.getId(); })); }
+		bool isInvited(User *usr) const { return (invited_.end() != std::find_if(invited_.begin(), invited_.end(), [&usr](const User& curr_cmp) { return usr->getId() == curr_cmp.getId(); })); }
+		bool isInvisble(User *usr) const { return (invisibles_.end() != std::find_if(invisibles_.begin(), invisibles_.end(), [&usr](const User& curr_cmp) { return usr->getId() == curr_cmp.getId(); })); }
+		bool isBanned(User *usr) const { return (banned_.end() != std::find_if(banned_.begin(), members_.end(), [&usr](const User& curr_cmp) { return usr->getId() == curr_cmp.getId(); })); }
 
 }; //Chanel
 
