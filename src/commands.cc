3ac3043ff39a3582612df944619d18998ad0e594
ayglazkov <AYGlazkov@stud.kpfu.ru>
2022-12-14 21:42:52 +0300

    cmd_nick

--- src/commands.cc
+++ src/commands.cc
@@ -1,47 +1,75 @@
 #include "../includes/Commands.hpp"
+#include <algorithm>
 #include <cstddef>
 #include <fstream>
 #include <string>
 #include <vector>
 
 int Commands::cmd_pass(std::vector<std::string> args, User* &user, Server *data) {
-    std::string msg;
-    if (args.size() < 2) {
-        msg = compileError(411, *user, args[0], "");
-        Server::sendMsg(user->getSockFd(), msg);
-        return 0;
-    }
-    if (user->getAccess()) {
-        msg = compileError(462, *user, "", "");
-        data->sendMsg(user->getSockFd(), msg);
-        return 0;
-    }
-    if (args[1] != data->getPassword()) {
-        msg = compileError(464, *user, "", "");
-        data->sendMsg(user->getSockFd(), msg);
-        data->disconnect(user);
-        return 1;
-    } else {
-        user->setAccess(1);
-    }
-    return 0;
+	std::string msg;
+	if (args.size() < 2) {
+		msg = compileError(411, *user, args[0], "");
+		Server::sendMsg(user->getSockFd(), msg);
+		return 0;
+	}
+	if (user->getAccess()) {
+		msg = compileError(462, *user, "", "");
+		data->sendMsg(user->getSockFd(), msg);
+		return 0;
+	}
+	if (args[1] != data->getPassword()) {
+		msg = compileError(464, *user, "", "");
+		data->sendMsg(user->getSockFd(), msg);
+		data->disconnect(user);
+		return 1;
+	} else {
+		user->setAccess(1);
+	}
+	return 0;
 }
 
 void Commands::cmd_motd(User *&user) {
-    std::ifstream file("motd/daily.msg");
-    std::string msg;
-    if (file) {
-        std::vector<std::string> vec = {"IRS_SERVER"};
-        msg = compileReply(375, *user, vec);
-        Server::sendMsg(user->getSockFd(), msg);
-        while (std::getline(file, vec[0])) {
-            msg = compileReply(372, *user, vec);
-            Server::sendMsg(user->getSockFd(), msg);
-        }
-        msg = compileReply(376, *user, vec);
-        Server::sendMsg(user->getSockFd(), msg);
-    } else {
-        msg = compileError(422, *user, "", "");
-        Server::sendMsg(user->getSockFd(), msg);
-    }
+	std::ifstream file("motd/daily.msg");
+	std::string msg;
+	if (file) {
+		std::vector<std::string> vec = {"IRS_SERVER"};
+		msg = compileReply(375, *user, vec);
+		Server::sendMsg(user->getSockFd(), msg);
+		while (std::getline(file, vec[0])) {
+			msg = compileReply(372, *user, vec);
+			Server::sendMsg(user->getSockFd(), msg);
+		}
+		msg = compileReply(376, *user, vec);
+		Server::sendMsg(user->getSockFd(), msg);
+	} else {
+		msg = compileError(422, *user, "", "");
+		Server::sendMsg(user->getSockFd(), msg);
+	}
+}
+
+void Commands::cmd_nick(std::vector<std::string> args, User *&user, std::vector<User *> &users) {
+	std::string msg;
+	if (args.size() != 2) {
+		msg = compileError(461, *user, args[0], "");
+		Server::sendMsg(user->getSockFd(), msg);
+		return;
+	}
+	if (std::any_of(users.begin(), users.end(), [&args](const User& us){return us.getNick() == args[1];})) {
+		msg = compileError(433, *user, args[1], "");
+		Server::sendMsg(user->getSockFd(), msg);
+		return;
+	}
+	if (std::any_of(args[1].begin(), args[2].end(),[](const char c){return c <= 32 || c > 126 || c == ' ' || c == ':' || c == '#' || c == '!' || c == '@';})) {
+		msg = compileError(432, *user, args[1], "");
+		Server::sendMsg(user->getSockFd(), msg);
+		return;
+	}
+	int flag_first = {};
+	if (user->getNick().empty()) {
+		flag_first = 1;
+	}
+	user->setNick(args[1]);
+	if (flag_first && user->getAutorization()) {
+		cmd_motd(user);
+	}
 }
